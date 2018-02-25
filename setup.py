df4ab3d3d6512b562c6255b282bcf14de20010c0
rasmusmunk <munk1@live.dk>
2018-02-26 00:49:37 +0100

    New refactor base versioning

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ else:
 
 setup(
     name='mig-utils',
-    version='0.0.2',
+    version='0.1.0',
     description='Minimum Intrusion Grid (MiG) Utilities library',
     author='Rasmus Munk',
     author_email='munk1@live.dk',
