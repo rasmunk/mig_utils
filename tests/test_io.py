cb504b60cc9220f6d06266aa74e804efc223a451
rasmusmunk <munk1@live.dk>
2018-03-23 17:03:53 +0100

    look for environ vars if file dosen't exist

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,15 +6,19 @@ sys.path.append("..")
 from mig.io import ErdaShare, IDMCShare
 
 # Test input
-with open('sharelinks.txt', 'r') as file:
-    content = file.readlines()
-if content is not None and len(content) > 0:
+try:
+    with open('sharelinks.txt', 'r') as file:
+        content = file.readlines()
+    assert content is not None
+    assert len(content) > 0
     sharelinks = dict((tuple(line.rstrip().split('=') for line in content)))
-else:
+except IOError:
     # Travis
+    assert 'ERDA_TEST_SHARE' in os.environ
+    assert 'IDMC_TEST_SHARE' in os.environ
+
     sharelinks = {'ERDA_TEST_SHARE': os.environ['ERDA_TEST_SHARE'],
                   'IDMC_TEST_SHARE': os.environ['IDMC_TEST_SHARE']}
-assert sharelinks is not None
 
 
 class ErdaShareTest(unittest.TestCase):
