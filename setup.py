07fa40612351228d1fd24abc86d1789f0a4dba7c
Rasmus Munk <munk1@live.dk>
2018-06-11 14:38:22 +0200

    tmp clone and build of ssh2-python

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 import sys
-from setuptools import find_packages
+from setuptools import find_packages, Extension
+from setuptools.command.install import install
+import subprocess
 # Python 2 install
 if sys.version_info[0] >= 3:
     from distutils.core import setup
 else:
     from setuptools import setup
+
+
+# TODO -> when ssh2-python 0.14.1 is release, switch to regular pip install
+class SSH2LibInstall(install):
+    def run(self):
+        command = 'git clone --recursive https://github.com/ParallelSSH/ssh2-python.git'
+        process = subprocess.Popen(command, shell=True, cwd='.')
+        process.wait()
+
+        build = 'git checkout a8e0d5d5da7dfd00346628b01ca07c61a5e4de1c' \
+                '&& sudo ./ci/install-ssh2.sh'
+        process = subprocess.Popen(build, shell=True, cwd='ssh2-python')
+        process.wait()
+        install.run(self)
+
 
 with open('README.rst') as r_file:
     long_description = r_file.read()
@@ -22,11 +39,9 @@ setup(
         'fs.sshfs>=0.8.0',
         'fs>=2.0.7',
         'six>=1.10',
-        'ssh2-python-0.14.1devel'
     ],
-    dependency_links=[
-        'git+git://github.com/ParallelSSH/ssh2-python.git@a8e0d5d5da7dfd00346628b01ca07c61a5e4de1c#egg=ssh2-python-0.14.1devel'
-    ],
+    cmdclass={'install': SSH2LibInstall},
+    include_package_data=True,
     tests_require=[
         'pytest',
     ]
