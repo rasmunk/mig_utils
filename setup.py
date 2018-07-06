56cf57f5d35abc283f4598dba0d4d279ec808ec5
Rasmus Munk <munk1@live.dk>
2018-07-06 14:57:49 +0200

    upgrade ssh2-python 0.15.0

--- setup.py
+++ setup.py
@@ -8,21 +8,6 @@ if sys.version_info[0] >= 3:
 else:
     from setuptools import setup
 
-
-# TODO -> when ssh2-python 0.14.1 is release, switch to regular pip install
-class SSH2LibInstall(install):
-    def run(self):
-        command = 'git clone --recursive https://github.com/ParallelSSH/ssh2-python.git'
-        process = subprocess.Popen(command, shell=True, cwd='.')
-        process.wait()
-
-        build = 'git checkout a8e0d5d5da7dfd00346628b01ca07c61a5e4de1c' \
-                '&& sudo ./ci/install-ssh2.sh'
-        process = subprocess.Popen(build, shell=True, cwd='ssh2-python')
-        process.wait()
-        install.run(self)
-
-
 with open('README.rst') as r_file:
     long_description = r_file.read()
 
@@ -38,9 +23,9 @@ setup(
     install_requires=[
         'fs.sshfs>=0.8.0',
         'fs>=2.0.7',
+        'ssh2-python>=0.15.0',
         'six>=1.10',
     ],
-    cmdclass={'install': SSH2LibInstall},
     include_package_data=True,
     tests_require=[
         'pytest',
